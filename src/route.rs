449bf539f12667d71ef267c88ca9470ab1860241
grety <snl1016@163.com>
2024-07-24 10:05:55 +0800

    u

--- src/route.rs
+++ src/route.rs
@@ -17,9 +17,30 @@ use mssql::*;
 //引入全局变量
 use crate::IS_WORKING;
 
-//引入用户类型
-pub mod interface;
-pub use interface::*;
+use serde::{Deserialize, Serialize}; //用于结构体上方的系列化宏
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LoginUser {
+    pub userName: String,
+    pub userPwd: String,
+    pub code: Option<i32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LoginResponse {
+    userName: String,
+    userPwd: String,
+    pub code: i32, // 0：成功，非0：失败
+}
+impl LoginResponse {
+    pub fn new(data: LoginUser, code: i32) -> Self {
+        LoginResponse {
+            code,
+            userName: data.userName,
+            userPwd: data.userPwd,
+        }
+    }
+}
 
 #[get("/?<phone>")]
 pub async fn phone(phone: String, pools: &State<Pool>) -> String {
